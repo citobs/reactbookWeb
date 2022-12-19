90e4f6f8d961dae0b93742a585a853a527e1e50a
citobs <altiorapetamus7@hanmail.net>
2022-12-19 23:21:48 +0900

    초기디자인

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [bookTitle, setBookTitle] = useState("");
+  const [bookPage, setBookPage] = useState("");
+  const [bookPublish, setBookPublish] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>앱매인임</h1>
+      <form>
+        <div>
+          <label htmlFor='book-title'>제목</label>
+          <input
+            type='text'
+            name='name'
+            id='book-title'
+            value={bookTitle}
+            placeholder='제목'
+            onChange={(e) => setBookTitle(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor='book-pages'>페이지수</label>
+          <input
+            type='number'
+            name='pages'
+            id='book-pages'
+            value={bookPage}
+            placeholder='제목'
+            onChange={(e) => setBookPage(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor='book-publish-date'>출판일</label>
+          <input
+            type='date'
+            name='publish-date'
+            id='book-publish-date'
+            value={bookPublish}
+            placeholder='제목'
+            onChange={(e) => setBookPublish(e.target.value)}
+          />
+        </div>
+      </form>
     </div>
   );
 }
