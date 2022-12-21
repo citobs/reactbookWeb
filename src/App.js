e2138728c1951fab6626791e582af9c861108025
citobs <altiorapetamus7@hanmail.net>
2022-12-21 15:56:24 +0900

    addbook.js추가

--- src/App.js
+++ src/App.js
@@ -1,107 +1,17 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
 import BookList from "./components/BookList";
-import db from "./firebase";
-import { Button, Field, Message, Modal } from "./ui";
+// import db from "./firebase";
+// import { Button, Field, Message, Modal } from "./ui";
 import "./styles.css";
+import AddBook from "./components/AddBook";
 
 function App() {
-  const [bookTitle, setBookTitle] = useState("");
-  const [bookPage, setBookPage] = useState("");
-  const [bookPublish, setBookPublish] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [isModal, setIsModal] = useState(false);
-  // const [books, setBooks] = useState(null);
-
-  //book데이터 확인
-  // console.log(books);
-
-  const handleBook = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    //firestore(데이터베이스)에 보내기(비동기설정 await)
-    try {
-      await db
-        .collection("books")
-        .add({
-          title: bookTitle,
-          page: parseInt(bookPage),
-          publish: new Date(bookPublish),
-          //에러메시지 추가
-        })
-        .then((docRef) => {
-          console.log(docRef);
-        });
-    } catch (e) {
-      setError("에러가 발생했습니다.");
-    }
-
-    // db들어가고 나서 초기화
-    setBookTitle("");
-    setBookPage("");
-    setBookPublish("");
-    setLoading(false);
-  };
-
   return (
     <div className='App'>
       <h1>CIT도서대출 프로그래밍!!!(리액트 겁나어렵)</h1>
 
-      <Button onClick={() => (setIsModal(true), setError(null))}>
-        {" "}
-        CIT 뉴북 추가하기!
-      </Button>
-
-      <Modal
-        title='cit 뉴북 책추가'
-        show={isModal}
-        close={() => setIsModal(false)}
-      >
-        <form>
-          <Field labelText='CIT도서제목' id='book-title'>
-            {/* <label htmlFor='book-title'>제목</label> */}
-            <input
-              type='text'
-              name='name'
-              id='book-title'
-              value={bookTitle}
-              placeholder='제목'
-              onChange={(e) => setBookTitle(e.target.value)}
-            />
-          </Field>
-          <Field labelText='페이지수' id='book-pages'>
-            {/* <label htmlFor='book-pages'>페이지수</label> */}
-            <input
-              type='number'
-              name='pages'
-              id='book-pages'
-              value={bookPage}
-              placeholder='페이지수'
-              onChange={(e) => setBookPage(e.target.value)}
-            />
-          </Field>
-          <Field labelText='출판일' id='book-publish-date'>
-            {/* <label htmlFor='book-publish-date'>출판일</label> */}
-            <input
-              type='date'
-              name='publish-date'
-              id='book-publish-date'
-              value={bookPublish}
-              placeholder='출판일'
-              onChange={(e) => setBookPublish(e.target.value)}
-            />
-          </Field>
-          {/* 삼항조건문추가 */}
-          {/* <button type='submit' onClick={handleBook} disabled={loading}>
-          {loading ? "저장중입니다..." : "저장하기"}
-        </button> */}
-          <Button type='submit' onClick={handleBook} disabled={loading}>
-            {loading ? "저장중입니다..." : "저장하기"}
-          </Button>
-          <Message text={error} type='error' />
-        </form>
-      </Modal>
+      <AddBook />
 
       <BookList />
     </div>
